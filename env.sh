e41c04ca334d8de66c3af7d07fbd0bd8ba6ed248
Mariam <mariam@lyon>
2023-03-24 15:02:51 +0100

    update env file

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export USERNAME=mahhttouche
 export DIR=/home/$USERNAME && export NODES_FILE=$DIR/kube_cluster_prep/nodesfile && export N_HOSTS=3 && \
-export PERIOD=8 && export CLUSTER_NAME=sagittaire && export MASTER_NODE=$(head -n 1 $NODES_FILE) && \
+export PERIOD=4 && export CLUSTER_NAME=nova && export MASTER_NODE=$(head -n 1 $NODES_FILE) && \
 export PROM_NODE=$(tail -1 $NODES_FILE)
